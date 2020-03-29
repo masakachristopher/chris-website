fa1c00477b0073cde45e64279bc8abf4efd4db02
masakachristopher <masakachristopher@yahoo.com>
2020-03-29 23:28:33 +0300

    initial commit

---
+++ about.php
@@ -0,0 +1,88 @@
+
+    
+<main>    
+    <div class=" d-flex justify-content-left ">
+                <div class="container">
+                    <div class="row">
+                        <div class="d-flex col-lg-5">
+                            <div class=" text-capitalize justify-content-left">
+                                <div class=" col-lg-12">
+                                    <h1>christopher masaka</h1>
+                                    <h3 class="text-uppercase">web developer</h3>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+         </div>
+</main>   
+    <div class="d-flex justify-content-center align-items-center p-2">
+            <div class="">
+                <div class="row d-flex justify-content-center">
+                    <!-- <div class="col-sm-1">
+                    </div> -->
+                    <div class="col-sm-2">
+                            <div class=" d-flex justify-content-left">
+                                <img src="assets/img/imgp.jpg" class="img-fluid" alt="">
+                            </div>
+                    </div>
+                    <div class="col-sm-6">
+                        <div class="blockquote" >
+                            <h2 class="headline mb-3 text-capitalize ">hello, I am chris!</h2>
+                                <p> Currently, I am learning<span><span class="blue text-uppercase"> web </span></span> <span>and</span><span class="blue text-uppercase"> android </span> Development.<br>
+                                    Also, I am bachelor's degree in Computer science student at <span class="pink">UDSM</span><br>
+                                    I deal with different web frameworks such as <span class="blue">bootstrap</span>, <span class="blue">google's flutter<span> and </span>php-laravel.</span>
+                                </p>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="d-flex justify-content-center p-2 ">
+                    
+                        <div class="col-7">
+                            <h2 class="text-uppercase d-flex justify-content-center">other interests</h2>
+                            <p class=""><span> Also, in networking I plan to deal with<span class="blue"> CCNA (Cisco Certified Network Associate). </span> 
+                             Is an information technology (IT) certification from Cisco Systems.</span> </p>
+
+                            <p>My concern with <span class="pink">CISCO </span> is all about knowledge for <span class="pink">routing</span>, <span class="pink">switching</span>  and <span class="pink">all wireless essentials</span>. Moreover, competence in <span class="text-uppercase blue">network security</span>. </p>
+                        </div>
+                </div>
+                <div>      
+                  <h2 class="text-uppercase d-flex justify-content-center">connect with me</h2>
+                    <div class="row d-flex justify-content-center">
+                        <div class="col-md-2 text-lowercase">
+                            <div class="wel_item d-flex justify-content-center">
+                                <a href="https://github.com/masakachristopher" class="social_link" title="github"><span>github</span></a>
+                            </div>
+                        </div> 
+                        <div class="col-md-2 text-lowercase">
+                            <div class="wel_item d-flex justify-content-center">
+                                <a href="https://www.instagram.com/christopher_masaka/?hl=en" class="social_link" title="instagram"><span>instagram</span></a>
+                            </div>
+                        </div> 
+                        <div class="col-md-2 text-lowercase">
+                            <div class="wel_item d-flex justify-content-center">
+                                <a href="https://developer.cisco.com/chrisdevnet" class="social_link" title="cisco DevNet"><span>cisco devnet</span></a>
+                            </div>
+                        </div> 
+                        <div class="col-md-2 text-lowercase">
+                            <div class="wel_item d-flex justify-content-center">
+                                <a href="#" class="social_link" title="twitter"><span>twitter</span></a>
+                            </div>
+                        </div> 
+                        
+                    </div>     
+                
+                </div>
+            
+            
+            </div>
+    </div>
+    
+</main>              
+                        
+                   
+                
+         
+
+       
