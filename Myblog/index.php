37d508e2e73ce2b0048f09145a19ac34cb8ef736
masakachristopher <masakachristopher@yahoo.com>
2020-03-29 19:33:31 +0300

    initial commit

---
+++ Myblog/index.php
@@ -0,0 +1,97 @@
+<!DOCTYPE html>
+<html class="fontawesome-i2svg-active fontawesome-i2svg-complete" lang="en">
+<head>
+    <link rel="shortcut icon" href="chrisweb.ico">
+    <link rel="shortcut icon" href="chrisweb.ico" type="image/x-icon">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>CM | Christopher Masaka, Developer</title>
+    <link href="assets/css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="assets/css/style.css">
+       
+
+</head>
+<header>
+    <!-- navigation bar -->
+    <div class="">
+        <nav class="navbar navbar-expand-lg navbar-dark navbar-custom ">
+            <div class="container">
+
+                <ul class="navbar-nav ml-auto text-left ">
+                    <li class="nav-item">
+                        <a class="nav-link navbar-brand" href="index.php?option=about">home</a>
+                    </li>
+                    <!-- <li class="nav-item">
+                        <a class="nav-link navbar-brand" href="index.php?option=blog">blog</a>
+                    </li> -->
+                    <li class="nav-item ">
+                        <a class="nav-link navbar-brand" href="index.php?option=project">projects</a>
+                    </li>
+                    <!-- <li class="nav-item">
+                        <a class="nav-link navbar-brand" href="index.php?option=other">More</a>
+                    </li> -->
+                </ul>
+                <div class="collapse navbar-collapse" id="navbarResponsive">         
+                <ul class="navbar-nav ml-auto text-right">
+                    <li class="nav-item">
+                        <a class="navbar-brand" href="#"><img class="img-fluid " height="95px" src="chrisweb.ico"  alt="logo"></a>
+                    </li>
+                   
+                </ul>
+                </div>  
+            </div>
+        </nav>
+    </div>
+    <!-- navigation bar ends -->
+</header>
+
+<body class="bg-black">
+    <div class="masthead-content">
+        <div class="responsive">
+            <div  class="d-flex justify-content-center">
+                <div   class="panel-body" >
+                    
+                        
+                    <?php 
+                        @$opt=$_GET['option'];
+                        
+                        if($opt!="")
+                        {
+                            
+                            if($opt=="about")
+                            {
+                            include('about.php');
+                    
+                            }
+                                                        
+                            else if($opt=="blog")
+                            {
+                            include('blog.php');
+                            }
+                            
+                            
+                            else if($opt=="project")
+                            {
+                            include('project.php');
+                            }
+                        }
+                        else
+                            include('home.php');
+                          
+                    ?>
+                        
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="d-flex justify-content-center">
+        <footer class="d-flex justify-content-center">
+           <span> <span>&copy 2019-<?php echo date("Y");?></span> , this site developed by chris</span>
+        </footer>
+    </div>
+      
+</body>
+   
+</html>
---
+++ Myblog/project.php
@@ -0,0 +1,23 @@
+<div class="d-flex justify-content-center">
+    <h2 class="h-title d-flex justify-content-center align-content-center">PROJECTS PAGE</h2>
+</div>
+<div>
+    <div class="row d-flex justify-content-center">
+        <div class="col-lg-10">
+          <h2>chris web project</h2>
+            <p>Built with PHP, bootstrap</p>
+            <span><a class="" href="#"><span class="source_btn"><span></span> Source</span></a></span>
+        </div>
+        <div class="col-lg-10">
+          <h2>uni news room</h2>
+            <p>Built with PHP, bootstrap</p>
+            <span><a class="" href="#"><span class="source_btn"><span></span> Source</span></a></span>
+        </div>
+        <div class="col-lg-10">
+          <h2>school app</h2>
+            <p>It is an android project. Built with firebase framework </p>
+            <span><a class="" href="#"><span class="source_btn"><span></span> Source</span></a></span>
+        </div>
+    </div>
+</div>
+
