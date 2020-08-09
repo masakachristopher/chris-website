04ef7efe9315a7704989e8accdd11128f9dc966c
masakachristopher <50275918+masakachristopher@users.noreply.github.com>
2020-08-09 11:42:45 +0300

    Update project.php

--- project.php
+++ project.php
@@ -7,11 +7,11 @@
             <p>PHP, Bootstrap</p>
             <span><a class="" href="#"><span class="source_btn"><span><i></i></span> Source</span></a></span>
         </div>
-        <div class="col-lg-10 ">
+<!--         <div class="col-lg-10 ">
           <h2 class="pink">uni news room</h2>
             <p>PHP, Bootstrap</p>
             <span><a class="" href="#"><span class="source_btn"><span><i></i></span> Source</span></a></span>
-        </div>
+        </div> -->
         <!-- <div class="col-lg-10  shell">
           <h2 class="pink">school app</h2>
             <p>An android project built with Java and Firebase </p>
