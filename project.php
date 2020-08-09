197d682561aa11da4ec98ea13d52369b957987d4
masakachristopher <50275918+masakachristopher@users.noreply.github.com>
2020-08-09 12:03:58 +0300

    Update project.php

--- project.php
+++ project.php
@@ -1,8 +1,8 @@
     <div class="d-flex justify-content-center">
         <h2 class="h-title d-flex justify-content-center align-content-center">projects</h2>
     </div>
-    <div class="row d-flex justify-content-center">
-        <div class="col-lg-10 ">
+    <div class="row d-flex justify-content-left">
+        <div class="col-lg-12 ">
           <h2 class="pink">chris website</h2>
             <p>PHP, Bootstrap</p>
             <span><a class="" href="https://github.com/masakachristopher/chris-website/"><span class="source_btn"><span><i></i></span> Source</span></a></span>
